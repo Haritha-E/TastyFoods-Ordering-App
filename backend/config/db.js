649877578d3f5b9fa18030d36170f50212848d27
Haritha-E <harithaeswaran@gmail.com>
2024-06-22 08:40:59 +0530

    ignore

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async() =>{
-    await mongoose.connect('mongodb+srv://harithaeswaran:2873@cluster0.xly3pm0.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-}
